a7e8ee4d97abcecc09f55dd4ede15508074810b4
zhangkaihongzkh <953628520@qq.com>
2016-10-20 21:24:57 +0800

    test

---
+++ 组件学习/7.[jQuery EasyUI]第7章 LinkButton(按钮)组件/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by 凯洪 on 2016/10/20.
+ */
+$(function(){
+  $('#box').linkbutton({
+    text: '提交',
+    toggle: true,
+    select: true,
+    iconCls: 'icon-add',
+    iconAlign: 'right'
+  })
+
+})
