1165d154dd164701f8db2098722258c18a147599
zhangkaihongzkh <953628520@qq.com>
2016-10-20 21:08:35 +0800

    test

---
+++ 组件学习/5.[jQuery EasyUI]第5章 Resizable(调整大小)组件/js/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by 凯洪 on 2016/10/20.
+ */
+$(function(){
+  
+  $('#rr').resizable({
+
+    maxWidth:800,
+    maxHeight:600,
+    handles:'se',
+    egde: 20
+  })
+  console.log($('#box').resizable('options'))
+})
