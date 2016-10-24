7011d595a9850958a5c93b27dabd36b72df9813d
zhangkaihongzkh <953628520@qq.com>
2016-10-24 17:17:50 +0800

    22.[jQuery EasyUI]第20章 SearchBox(搜索框)组件

---
+++ 组件学习/22.[jQuery EasyUI]第20章 SearchBox(搜索框)组件/js/index.js
@@ -0,0 +1,12 @@
+﻿/**
+ * Created by 凯洪 on 2016/10/20.
+ */
+$(function(){
+  $('#box').searchbox({
+    searcher: function(value, name){
+      alert(value + '-' + name);
+    },
+    menu: '#box',
+    prompt: '请输入内容',
+  })
+})
