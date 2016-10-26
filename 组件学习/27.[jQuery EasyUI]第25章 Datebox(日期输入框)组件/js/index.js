ab4a5d52b375de2e224a928d90dc381723e29191
zhangkaihongzkh <953628520@qq.com>
2016-10-26 16:31:06 +0800

    27.[jQuery EasyUI]第25章 Datebox(日期输入框)组件

---
+++ 组件学习/27.[jQuery EasyUI]第25章 Datebox(日期输入框)组件/js/index.js
@@ -0,0 +1,25 @@
+﻿/**
+ * Created by 凯洪 on 2016/10/20.
+ */
+$(function(){
+  var buttons = $.extend([], $.fn.datebox.defaults.buttons);
+  buttons.splice(1, 0, {
+    text: 'mybtn',
+    handler: function(){
+      alert( 'mybtn')
+    }
+  });
+  $('#box').datebox({
+    panelWidth: 210,
+    panelHeight: 210,
+    buttons: buttons,
+  });
+
+  $('#box').datebox('calendar').calendar({
+    firstDay: 1,
+  })
+
+
+
+
+})
