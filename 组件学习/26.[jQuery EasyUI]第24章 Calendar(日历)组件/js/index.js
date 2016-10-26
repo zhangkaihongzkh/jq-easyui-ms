a06f51ac31a236538b80af9e9a48f213bf6bc255
zhangkaihongzkh <953628520@qq.com>
2016-10-26 16:21:27 +0800

    26.[jQuery EasyUI]第24章 Calendar(日历)组件

---
+++ 组件学习/26.[jQuery EasyUI]第24章 Calendar(日历)组件/js/index.js
@@ -0,0 +1,10 @@
+﻿/**
+ * Created by 凯洪 on 2016/10/20.
+ */
+$(function(){
+  $('#box').calendar({
+    width: 210,
+    height: 210,
+  });
+  $('#box').calendar('moveTo', new Date(2016,10,26))
+})
