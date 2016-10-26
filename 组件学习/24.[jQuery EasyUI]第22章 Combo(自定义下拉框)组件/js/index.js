c91d565a9ace394c6ac13a187c03d658ade06423
zhangkaihongzkh <953628520@qq.com>
2016-10-26 16:11:34 +0800

    24.[jQuery EasyUI]第22章 Combo(自定义下拉框)组件

---
+++ 组件学习/24.[jQuery EasyUI]第22章 Combo(自定义下拉框)组件/js/index.js
@@ -0,0 +1,16 @@
+﻿/**
+ * Created by 凯洪 on 2016/10/20.
+ */
+$(function(){
+  $('#box').combo({
+    required: true,
+
+  });
+  $('#food').appendTo($('#box').combo('panel'));
+
+  $('#food input').click(function(){
+    var v = $(this).val();
+    var s = $(this).next('span').text();
+    $('#box').combo('setValue', v).combo('setText', s).combo('hidePanel');
+  })
+})
